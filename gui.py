2022f499e29b714e4f942f788edffb7f4fefd2b4
Akshat Patil <akshatpatil7@gmail.com>
2023-08-18 12:57:47 -0500

    modified gui and added a batch file to process

--- gui.py
+++ gui.py
@@ -6,7 +6,73 @@ import glob
 import electrofacies as ef
 from tqdm import tqdm
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QCheckBox, QVBoxLayout, QWidget, QFileDialog
+import threading
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QCheckBox, QVBoxLayout, QWidget, QFileDialog, QProgressBar, QStatusBar
+
+class ProcessingThread(threading.Thread):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+
+    def run(self):
+        source_folder = self.parent.source_folder_input.text()
+        dest_folder = self.parent.dest_folder_input.text()
+
+        precondition = self.parent.precondition_checkbox.isChecked()
+        fluidprop = self.parent.fluidprop_checkbox.isChecked()
+        multimineral = self.parent.multimineral_checkbox.isChecked()
+        electrofacies = self.parent.electrofacies_checkbox.isChecked()
+
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+
+        las_files = glob.glob(os.path.join(source_folder, '*.las'))
+
+        processed_files = []
+
+        for index, las_file in enumerate(las_files):
+            try:
+                log = pet.Log(las_file)
+
+                if precondition:
+                    log.precondition()
+                if fluidprop:
+                    log.fluid_properties()
+                if multimineral:
+                    log.multimineral_model()
+
+                well_name = log.well['UWI'].value.replace('.', '')
+                processed_las_file = os.path.join(dest_folder, f'{well_name}_processed.las')
+                log.write(processed_las_file)
+
+                processed_files.append(processed_las_file)
+
+            except Exception as e:
+                print(f"An error occurred while processing {las_file}: {str(e)}")
+                logging.error(f"An error occurred while processing {las_file}: {str(e)}")
+
+            self.parent.update_progress(index + 1, len(las_files))
+
+            # Update the status bar to show progress like "1/4 files"
+            self.parent.update_status_message(f"Processing {index}/{len(las_files)} files")
+
+        if electrofacies:
+            logs = [pet.Log(x) for x in processed_files]
+
+            # Include the electrofacies processing step in the progress bar
+            self.parent.update_progress(len(las_files) + 1, len(las_files) + 1)
+
+            combined_logs = ef.electrofacies(logs=logs,)
+
+            for i, log in enumerate(combined_logs):
+                log.write(processed_files[i])  # Overwrite the processed file with electrofacies results
+
+            # Update the status bar for electrofacies step
+            self.parent.update_status_message(f"Processing Electrofacies (Step {len(las_files) + 1}/{len(las_files) + 1})")
+
+        self.parent.processing_completed()
+        # Update the status bar to show completion status
+        self.parent.update_status_message("Processing completed!")
 
 class ProcessingGUI(QMainWindow):
     def __init__(self):
@@ -50,7 +116,15 @@ class ProcessingGUI(QMainWindow):
         self.setCentralWidget(widget)
 
         self.setWindowTitle('Log Processing GUI')
-        self.setGeometry(100, 100, 400, 300)
+        self.setGeometry(200, 200, 400, 300)
+
+        # Create and set up the status bar
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+
+        # Create and set up the progress bar
+        self.progress_bar = QProgressBar()
+        self.status_bar.addWidget(self.progress_bar)
 
     def browseSourceFolder(self):
         folder = QFileDialog.getExistingDirectory(self, 'Select Source Folder')
@@ -61,62 +135,26 @@ class ProcessingGUI(QMainWindow):
         self.dest_folder_input.setText(folder)
 
     def processFiles(self):
-        source_folder = self.source_folder_input.text()
-        dest_folder = self.dest_folder_input.text()
+        self.process_button.setEnabled(False)  # Disable the process button while processing
+        self.progress_bar.setValue(0)
+        self.progress_bar.setMaximum(1)  # Set a temporary max value
 
-        precondition = self.precondition_checkbox.isChecked()
-        fluidprop = self.fluidprop_checkbox.isChecked()
-        multimineral = self.multimineral_checkbox.isChecked()
-        electrofacies = self.electrofacies_checkbox.isChecked()
+        # Create an instance of the processing thread and start it
+        self.process_thread = ProcessingThread(self)
+        self.process_thread.start()
 
-        if not os.path.exists(dest_folder):
-            os.makedirs(dest_folder)
+    def update_progress(self, value, max_value):
+        self.progress_bar.setMaximum(max_value)
+        self.progress_bar.setValue(value)
 
-        las_files = glob.glob(os.path.join(source_folder, '*.las'))
+    def update_status_message(self, message):
+        self.status_bar.showMessage(message)
 
-        # Initialize a status bar for processing
-        progress_bar = tqdm(total=len(las_files), desc="Processing LAS files")
+    def processing_completed(self):
+        self.progress_bar.setValue(self.progress_bar.maximum())
+        self.process_button.setEnabled(True)  # Re-enable the process button
 
-        processed_files = []
-
-        for las_file in las_files:
-            try:
-                log = pet.Log(las_file)
-
-                if precondition:
-                    log.precondition()
-                if fluidprop:
-                    log.fluid_properties()
-                if multimineral:
-                    log.multimineral_model()
-
-                well_name = log.well['UWI'].value.replace('.', '')
-                processed_las_file = os.path.join(dest_folder, f'{well_name}_processed.las')
-                log.write(processed_las_file)
-
-                processed_files.append(processed_las_file)
-
-            except Exception as e:
-                print(f"An error occurred while processing {las_file}: {str(e)}")
-                logging.error(f"An error occurred while processing {las_file}: {str(e)}")
-
-            progress_bar.update()
-
-        progress_bar.close()
-
-        if electrofacies:
-            logs = [pet.Log(x) for x in processed_files]
-
-            progress_bar_ef = tqdm(total=len(logs), desc="Applying Electrofacies")
-            combined_logs = ef.electrofacies(logs=logs)
-
-            for i, log in enumerate(combined_logs):
-                log.write(processed_files[i])  # Overwrite the processed file with electrofacies results
-                progress_bar_ef.update()
-
-            progress_bar_ef.close()
-
-        print("Processing completed!")
+# ... (Your existing __main__ block)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
